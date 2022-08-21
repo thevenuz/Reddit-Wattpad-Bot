6490cf7030034973404bccaddd7e540e72d3febd
sagarambati <sagarambati734@gmail.com>
2022-08-21 23:14:12 +0530

    reddit wattpad bot initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const Snoowrap = require("snoowrap");
+
+const {CommentStream} = require("snoostorm");
+
+const BOT_START = Date.now() / 1000;
+
+try{
+    
+    const r = new Snoowrap({
+        userAgent: 'reddit-wattpad-bot-test',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        username: process.env.REDDIT_USER,
+        password: process.env.REDDIT_PASS
+    });
+
+    const canSummon = (msg) => {
+        return msg && msg.toLowerCase().includes('u/wattpadbot');
+    };
+
+    const stream = new CommentStream(r, { subreddit: "mytestingspacewpad", results: 1 });
+
+    console.log("triggered....");
+
+    // comments.on('item', (item) => {
+    //     console.log(item);
+    // });
+
+    stream.on("item", comment => {
+        console.log("reached");
+        console.log(comment);
+        if(comment.created_utc < BOT_START) return;
+        if(!canSummon(comment.body)) return;
+        comment.reply("reply from bot!");
+});
+}
+
+catch (ex){
+    console.log(ex);
+}
