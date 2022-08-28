f18e9477161dec123f8bc8a620097c01c29cc323
sagarambati <sagarambati734@gmail.com>
2022-08-28 23:17:36 +0530

    add scraping and module binding logic

--- app.js
+++ app.js
@@ -4,38 +4,65 @@ const Snoowrap = require("snoowrap");
 
 const {CommentStream} = require("snoostorm");
 
+const wattpad = require("./wattpad.js");
+
 const BOT_START = Date.now() / 1000;
 
 try{
-    
-    const r = new Snoowrap({
-        userAgent: 'reddit-wattpad-bot-test',
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        username: process.env.REDDIT_USER,
-        password: process.env.REDDIT_PASS
-    });
+    // (async () => {
 
-    const canSummon = (msg) => {
-        return msg && msg.toLowerCase().includes('u/wattpadbot');
-    };
-
-    const stream = new CommentStream(r, { subreddit: "mytestingspacewpad", results: 1 });
-
-    stream.on("item", comment => {
-        if(!comment.created_utc < BOT_START && canSummon(comment.body))
-        {
-            response = getCommentData(comment.body);
-            console.log(response);
-            comment.reply(response);
-        }
+        const r = new Snoowrap({
+            userAgent: 'reddit-wattpad-bot-test',
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            username: process.env.REDDIT_USER,
+            password: process.env.REDDIT_PASS
         });
+    
+        const canSummon = (msg) => {
+            return msg && msg.toLowerCase().includes('u/wattpadbot');
+        };
+    
+        const stream = new CommentStream(r, { subreddit: "mytestingspacewpad", results: 1 });
+    
+        stream.on("item", comment => {
+            if(comment.created_utc > BOT_START && canSummon(comment.body))
+            {
+                response = getCommentData(comment.body);
+                replyBody = buildCommentReply(response);
+                console.log(replyBody);
+                // comment.reply(response);
+            }
+            });
+    
+        const getCommentData = (comment) => {
+            const re= /{([\w-]+( [\w]+)*)[ ]?}/gm;
+            comment = comment.replace("u/wattpadbot", "");
+            const matches = comment.matchAll(re);
+            const storyTitles= [];
 
+            for (const match of matches) {
+                storyTitles.push(match[1])
+            }
+            return storyTitles;
+        };
 
-    const getCommentData = (comment) => {
-        return comment.replace("u/wattpadbot", "");
-    };
-}
+        
+        const buildCommentReply = (storyTitles) => {
+            storyTitles.forEach( title => {
+                wattpad(title).then( res => {
+                    if(res){
+                        return res;
+                    }
+                });
+            });
+        };
+        
+        // const stories=await wattpad("testuserwp");
+
+        // console.log("stoies:", stories, "stories");
+    }
+
 
 catch (ex){
     console.error(ex);
--- wattpad.js
+++ wattpad.js
@@ -1,27 +1,44 @@
 const axios= require("axios");
 const cheerio = require("cheerio");
 
-const headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'};
-const domain = "https://www.wattpad.com/search/";
+try{    
+    const headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'};
+    const searchPrefix = "https://www.wattpad.com/search/";
+    const domain = "https://www.wattpad.com";
+    
+    const getStoryLink= async (storyTitle) => {
+        const searchLink = searchPrefix + storyTitle;
+        const responseData=[];
 
-axios.get(domain, {
-    headers: headers
-})
-.then((res) => {
-    const htmldata= res.data;
-    const $ = cheerio.load(htmldata);
+        const res = await axios.get(searchLink, {
+            headers: headers
+        });
+        const htmldata= res.data;
+        const $ = cheerio.load(htmldata);
+        console.log("get story called");
+        const values = $('.story-card-container .list-group-item');
 
-    const values= $('.story-card-container .list-group-item').each((index, element) => {
-        const URL= $(element).children('a').attr('href');
-        const title= $(element).children('.story-card').children('.story-card-data').children('.top-section').children('.story-info').children('.title').text();
-        console.log(title)
-        console.log(URL)
-    });
-})
-.catch((err) => {
-    console.log(err);
-});
+        for (const value of values)
+        {
+            const storyURL= $(value).children('a').attr('href');
+            const title= $(value).children('.story-card').children('.story-card-data').children('.story-info').children('.title').text();
+            const jsonData = {
+                "title": title,
+                "url" : domain + storyURL
+            };
 
-scrape();
+            responseData.push(jsonData);
+        }
+
+    }   
+
+    module.exports = getStoryLink;
+}
+
+catch (ex){
+    console.log(ex);
+}
+
+
 
 
