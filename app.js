235c055fb0afa0d0956d06f5bb5402591cc834ef
sagarambati <sagarambati734@gmail.com>
2022-08-31 23:22:53 +0530

    added author command logic

--- app.js
+++ app.js
@@ -26,11 +26,39 @@ try{
         stream.on("item", async (comment) => {
             if(comment.created_utc > BOT_START && canSummon(comment.body))
             {
-                response = getCommentData(comment.body);
-                const reply = await buildCommentReply(response);
+                const stories = getStoriesFromComment(comment.body);
+                const authors = getAuthorsFromComment(comment.body);
+                //response = getCommentData(comment.body);
+                const reply = await buildCommentReply(stories, authors);
                 comment.reply(reply);
             }
             });
+
+        const getStoriesFromComment = (comment) => {
+            const re= /{([\w-]+( [\w]+)*)[ ]?}/gm;
+            comment = comment.replace("u/wattpadbot", "");
+            console.log(comment);
+            const matches = comment.matchAll(re);
+            const storyTitles= [];
+
+            for (const match of matches) {
+                storyTitles.push(match[1])
+            }
+            return storyTitles;
+        };
+
+        const getAuthorsFromComment = (comment) => {
+            const re= /<([\w-]+( [\w]+)*)[ ]?>/gm;
+            comment = comment.replace("u/wattpadbot", "");
+            console.log(comment);
+            const matches = comment.matchAll(re);
+            const authorNames= [];
+
+            for (const match of matches) {
+                authorNames.push(match[1])
+            }
+            return authorNames;
+        };
     
         const getCommentData = (comment) => {
             const re= /{([\w-]+( [\w]+)*)[ ]?}/gm;
@@ -45,16 +73,45 @@ try{
             return storyTitles;
         };
 
-        const buildCommentReply = async (storyTitles) => {
+        const buildCommentReply = async (storyTitles, authorNames) => {
             const resData=[];
             let replyString="";
             for (const title of storyTitles){
-                const res= await wattpad(title);
+                const res= await wattpad.getStoryData(title);
                 // resData.push(res); 
                 replyString =replyString + `**${res["title"]}** - [link](${res["url"]})\n\n^status: ^${res["status"]} ^| ^Reads: ^${res["stats"]["reads"]} ^| ^Votes: ^${res["stats"]["votes"]} ^| ^Chapters: ^${res["stats"]["parts"]}\n\n\n\n`;
             }
+
+            for (const author of authorNames)
+            {
+                const authorResult= await wattpad.getAuthorData(author);
+                console.log(authorResult);
+
+                replyString =replyString + `**${authorResult["name"]}** - [link](${authorResult["url"]})\n\n^Stories: ^${authorResult["stats"]["stories"]} ^| ^Followers: ^${authorResult["stats"]["followers"]}\n\n\n\n`;
+            }
+
+            replyString = addFooter(replyString);
             return replyString;
         };
+
+        const addFooter = (reply) => {
+            reply = reply + "\n\n___\n\n";
+            const footer = "^commands: ^{story}, ^<author>";
+            reply = reply + footer;
+
+            return reply;
+        };
+
+        // const buildCommentReply = async (storyTitles) => {
+        //     const resData=[];
+        //     let replyString="";
+        //     for (const title of storyTitles){
+        //         const res= await wattpad(title);
+        //         // resData.push(res); 
+        //         replyString =replyString + `**${res["title"]}** - [link](${res["url"]})\n\n^status: ^${res["status"]} ^| ^Reads: ^${res["stats"]["reads"]} ^| ^Votes: ^${res["stats"]["votes"]} ^| ^Chapters: ^${res["stats"]["parts"]}\n\n\n\n`;
+        //     }
+        //     return replyString;
+        // };
     }
 
 
--- wattpad.js
+++ wattpad.js
@@ -6,7 +6,7 @@ try{
     const searchPrefix = "https://www.wattpad.com/search/";
     const domain = "https://www.wattpad.com";
     
-    const getStoryLink= async (storyTitle) => {
+    const getStoryData= async (storyTitle) => {
         const searchLink = searchPrefix + storyTitle;
         const responseData=[];
 
@@ -44,9 +44,53 @@ try{
             // responseData.push(jsonData);
         }
         // return responseData;
-    }   
+    };  
 
-    module.exports = getStoryLink;
+    const getAuthorData = async (authorName) => {
+        const authorSearchLink = searchPrefix + authorName;
+
+        const authorSearchResult = await axios.get(authorSearchLink, {
+            headers: headers
+        });
+        const authorHtmlData= authorSearchResult.data;
+        const $ = cheerio.load(authorHtmlData);
+
+        const authorsSection = $('#section-results-people').find('#results-people').find('#results-people-region').find('.feed-item-new.panel').children('.component-wrapper');
+
+        const authorsList = $(authorsSection).children('.list-group').children('.list-group-item');
+
+        for (const author of authorsList)
+        {
+            const authorProfile = $(author).children('.profile-card');
+            const authorProfileURL = $(authorProfile).children('a').attr('href');
+            const authorName = $(authorProfile).children('.profile-card-data').children('.card-content').children('.name-and-badges').children('h5').text();
+            const authorStats = $(authorProfile).children('.profile-card-data').children('.card-content').children('.card-meta').children();
+
+            const authorStatsData = {};
+
+            for (const stat of authorStats)
+            {
+                const authorStatType = $(stat).text().split(" ")[1].toLowerCase();
+                const authorStatValue = $(stat).children().text();
+
+                authorStatsData[authorStatType] = authorStatValue;
+            }
+
+            const authorResponseData = {
+                "name" : authorName,
+                "url" : domain + authorProfileURL,
+                "stats" : authorStatsData
+            };
+
+            return authorResponseData;
+        }
+
+    };
+
+    module.exports = {
+        getStoryData,
+        getAuthorData
+    };
 }
 
 catch (ex){
